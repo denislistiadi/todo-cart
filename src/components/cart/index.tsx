0848b26176236a868028773c0757eff115f779d7
Denis Listiadi <denislistiadi24@gmail.com>
2025-03-14 21:17:21 +0700

    Add empty cart state message and conditional rendering

--- src/components/cart/index.tsx
+++ src/components/cart/index.tsx
@@ -1,13 +1,21 @@
+import useCart, { ICartStore } from '../../store/cart.store';
 import Header from '../Header';
 import CartList from './cart-list';
 
 export default function Index() {
+  const { cart } = useCart((state: ICartStore) => state);
   return (
     <>
       <Header />
       <main className="container">
         <h2>Cart List</h2>
-        <CartList />
+        {cart.length > 0 ? (
+          <CartList />
+        ) : (
+          <p className="empty-cart">
+            Your cart is empty. Please add items in the product list.
+          </p>
+        )}
       </main>
     </>
   );
